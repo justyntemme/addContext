f74d30518a1be63fb4c75065f009431d7359c625
Justyn Temme <justyntemme@gmail.com>
2023-03-18 12:16:41 -0500

    Use -file to add a file instead of a git repo

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ import (
 func cloneRepo(url string) (string, error) {
 	// Create a temporary directory
 	tempDir, err := ioutil.TempDir("", "repoToAdd")
+
+	defer os.RemoveAll(tempDir)
 	if err != nil {
 		fmt.Println("Error creating temporary directory:", err)
 		return "", err
@@ -46,7 +48,9 @@ func main() {
 
 	// add the text and key flags to the command
 	c.Flags().StringP("url", "u", "", "the repositoy to evaluate")
-	c.Flags().StringP("question", "q", "", "question to ask about code")
+	c.Flags().StringP("question", "q", "", "question to ask")
+
+	c.Flags().StringP("file", "f", "", "File to add in chat")
 	c.Flags().StringP("key", "k", "", "OpenAI API key")
 	err := c.Execute()
 	if err != nil {
@@ -73,16 +77,29 @@ func getApiKey(c *cobra.Command) string {
 func addContext(c *cobra.Command, args []string) {
 	// get the API key from the command parameter or the environment variable
 	apiKey := getApiKey(c)
+	text := []byte("")
 	// get the text argument from the command
 	url, err := c.Flags().GetString("url")
-	tempDir, err := cloneRepo(url)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	if url != "" {
+		tempDir, err := cloneRepo(url)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		text, err = readGoFiles(tempDir)
+	} else {
+		file, err := c.Flags().GetString("file")
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		textbytes, err := readfile(file)
+		text = []byte(textbytes)
+
 	}
 	question, err := c.Flags().GetString("question")
 
-	text, err := readGoFiles(tempDir)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -102,12 +119,13 @@ func addContext(c *cobra.Command, args []string) {
 	fmt.Println(resp.Object)
 	fmt.Println(resp.Data[0].Embedding)
 	var responses []gpt3.ChatCompletionResponse
+	text = []byte("This is a csv of sports data, read it and use the data to answer the following question from the user, if you can't figure it out from the data, simply say i don't know")
 	chatResp, err := client.ChatCompletion(ctx, gpt3.ChatCompletionRequest{
 		Model: gpt3.GPT3Dot5Turbo0301,
 		Messages: []gpt3.ChatCompletionRequestMessage{
 			{
 				Role:    "system",
-				Content: "You are a teacher taking questions from students about their code, this is the given code `" + string(text) + string("`"),
+				Content: string(text),
 			},
 			{
 				Role:    "user",
@@ -128,7 +146,24 @@ func addContext(c *cobra.Command, args []string) {
 	}
 
 	fmt.Print(output.String())
-	defer os.RemoveAll(tempDir)
+}
+
+func readfile(filepath string) (string, error) {
+	// Open the file at the given file path
+	file, err := os.Open(filepath)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	// Read the contents of the file into a byte slice
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	// Convert the byte slice to a string and return it
+	return string(bytes), nil
 }
 
 func readGoFiles(dirPath string) ([]byte, error) {
